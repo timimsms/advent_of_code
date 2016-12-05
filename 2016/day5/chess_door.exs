7cdc2bdff46b9a17771ce3142dab01984af9c3ae
Tim Walsh <tim@useed.org>
2016-12-05 06:13:41 -0700

    Update for Part II
    Most of the Part II solution. Will work out the remaining issues later :+1:

--- 2016/day5/chess_door.exs
+++ 2016/day5/chess_door.exs
@@ -1,13 +1,14 @@
-require IEx
-
 # Advent of Code - Day 5: How About a Nice Game of Chess?
 # Author: Tim Walsh (c) 2016
 #
-# Elixir solutions for Day 5.
+# Elixir solutions for Day 5 (Parts I & II).
 # Run solutions via `iex chess_door.exs`.
 #
-# Solution:
-# => ChessDoor.crack("uqwqemis", 0, "")
+# Part I Solution:
+# => ChessDoor.crack_easy("uqwqemis", 0, "")
+#
+# Part II Solution:
+# => ChessDoor.crack_hard("uqwqemis")
 defmodule ChessDoor do
   # Generates an MD5 hash for our purposes.
   def hash(str) do
@@ -17,11 +18,12 @@ defmodule ChessDoor do
 
   # Use Regex to determine if there is a valid key.
   def check_hash(hash_str) do
-    Regex.named_captures(~r/\b(0{5})(?<key>.)(.*)/, hash_str)
+    Regex.named_captures(~r/\b(0{5})(?<key>.)(?<position>.)(.*)/, hash_str)
   end
 
-  # Runs cracking algorithm based on provided input.
-  def crack(input, i, result) do
+  # Runs cracking algorithm based on provided input
+  # on the "easy" door (i.e., Part I).
+  def crack_easy(input, i, result) do
     if String.length(result) >= 8 do
       result
     else
@@ -32,10 +34,31 @@ defmodule ChessDoor do
         key = check["key"]
         IO.puts "#{attempt} => #{hash_str}"
         IO.puts "key found! ~> #{key}"
-        crack(input, (i + 1), "#{result}#{key}")
+        crack_easy(input, (i + 1), "#{result}#{key}")
       else
-        crack(input, (i + 1), result)
+        crack_easy(input, (i + 1), result)
       end
+    end
+  end
+
+  # Runs cracking algorithm based on provided input
+  # on the "hard" door (i.e., Part II).
+  def crack_hard(input, i \\ 0, result \\ "________") do
+    unless String.match?(result, ~r/_/) do
+      result
+    else
+      check = check_hash(hash("#{input}#{i}"))
+      unless is_nil(check) do
+        result_ary = String.graphemes(result)
+        key = check["key"]
+        pos = String.to_integer(check["position"], 16)
+        if (pos <= 7) && (Enum.at(result_ary, pos) == "_") do
+          new_result = (result_ary |> List.replace_at(pos, key) |> Enum.join)
+          IO.puts new_result
+          crack_hard(input, (i + 1), new_result)
+        end  
+      end
+      crack_hard(input, (i + 1), result)
     end
   end
 end
