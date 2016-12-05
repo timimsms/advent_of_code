0b79dd1f501ef6b757364291f2f44131983c8db0
Tim Walsh <tim@useed.org>
2016-12-05 03:08:17 -0700

    Part 1 Solution :raised_hands:
    Getting more familiar with the Elixir syntax.

--- 2016/day4/sector_checker.exs
+++ 2016/day4/sector_checker.exs
@@ -1,5 +1,3 @@
-require IEx
-
 # Advent of Code - Day 4: Security Through Obscurity
 # Author: Tim Walsh (c) 2016
 #
@@ -12,6 +10,7 @@ defmodule SectorChecker do
   defmodule Sector do
     defstruct [:name, :sector_id, :checksum]
 
+    # Build Sector from map of corresponding fields.
     def build(map) do
       %Sector{
         name: Map.fetch!(map, "name"),
@@ -20,6 +19,7 @@ defmodule SectorChecker do
       }
     end
 
+    # Parse a string into a Sector object.
     def parse(str) do
       Regex.named_captures(
         ~r/(?<name>.*-)(?<sector_id>\d*)\[(?<checksum>.*)\]/,
@@ -27,35 +27,56 @@ defmodule SectorChecker do
       ) |> Sector.build
     end
 
-    # Recuresively generate a checksum from a list of letters
-    def generate_checksum([head | tail], map, checksum) do
-      if Kernel.length(checksum) < 5 do
-        IEx.pry
+    # Recuresively generate a checksum from a scored list of letters
+    def gen_checksum([], checksum_array) do
+      Enum.join(checksum_array)
+    end
+    def gen_checksum([head | tail], checksum_array) do
+      if length(checksum_array) >= 5 do
+        Enum.join(checksum_array)
       else
-
+        key = head |> Tuple.to_list |> List.first
+        gen_checksum(
+          tail, 
+          List.insert_at(
+            checksum_array,
+            length(checksum_array),
+            key
+          )
+        )
       end
     end
-    
-    def generate_checksum([], map, checksum) do
-      checksum
+
+    # Custom reduce method based on letter counts
+    def reduce_by_letter_count(enumerable) do
+      enumerable
+      |> Enum.reduce(%{}, fn (x, acc) ->
+        if Map.has_key?(acc, x) do
+          Map.update!(acc, x, &(&1 + 1))
+        else
+          Map.put_new(acc, x, 1)
+        end
+      end)
+    end
+
+    # Transform an array of letters to a 
+    def name_to_sorted_list(name) do
+      String.replace(name, "-", "")
+      |> String.graphemes
+      |> Enum.map(fn c -> String.to_atom(c) end)
+      |> Sector.reduce_by_letter_count
+      |> Map.to_list
+      |> Enum.sort_by(fn(a) -> List.last(Tuple.to_list a) end, &>=/2)
     end
 
     def valid?(sector) do
-      String.replace(sector.name, "-", "")
-      |> String.graphemes
-      |> Enum.map(fn c -> String.to_atom(c) end)
-      |> Enum.reduce(%{},
-        fn (x, acc) ->
-          if Map.has_key?(acc, x) do
-            Map.update!(acc, x, &(&1 + 1))
-          else
-            Map.put_new(acc, x, 1)
-          end
-        end)
-      |> Sector.generate_checksum("")
+      Sector.name_to_sorted_list(sector.name)
+      |> Sector.gen_checksum([])
+      |> String.equivalent?(sector.checksum)
     end
   end
 
+  # SectorChecker.load_sector_data("./data/input.txt")
   def load_sector_data(filename) do
     File.stream!(filename)
     |> Enum.map(fn(str) -> Sector.parse(str) end)
@@ -64,15 +85,12 @@ defmodule SectorChecker do
   # SectorChecker.sum_valid_sector_ids("./data/input.txt")
   def sum_valid_sector_ids(filename) do
     load_sector_data(filename)
-    |> Enum.reduce(0, fn(sector, acc) -> 
-      if Sector.valid?(sector), do: acc + sector.sector_id
+    |> Enum.reduce(0, fn(sector, acc) ->
+      if Sector.valid?(sector) do
+        acc + sector.sector_id
+      else
+        acc
+      end
     end)
-  end
-
-  # SectorChecker.test_valid?
-  def test_valid? do
-    "not-a-real-room-404[oarel]"
-    |> Sector.parse
-    |> Sector.valid?
   end
 end
